3c9866aa2a54e1b3046bdba818c31ff0cfea35ab
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2023-12-01 16:42:15 -0800

    Added form with inputs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import { getAccessMapLink, getNavigation } from './directions';
-import { Search } from './Search';
 import GetDirectionsButton from './GetDirectionsButton'; 
+import DirectionOptions from './DirectionOptions';
 
 const start = [-122.30505981879918, 47.65331281821765 ]
 const end = [-122.31176697575529, 47.654660809752976 ]
@@ -10,16 +9,11 @@ const end = [-122.31176697575529, 47.654660809752976 ]
 const startObject = {longitude: -122.30505981879918, latitude: 47.65331281821765}
 const endObject = {longitude:-122.31176697575529, latitude: 47.654660809752976}
 function App() {
-  getNavigation(start, end).then((res) => console.log(res));
-
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-        <Search/>
-        </form>
+        <DirectionOptions />
         <a href={getAccessMapLink(startObject, endObject)} target="_blank">Directions</a>
-        <GetDirectionsButton />
       </header>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,20 @@
 import { SearchBox } from '@mapbox/search-js-react';
 const token = require('./token.js');
 
-export function Search() {
+// Takes in a handleRetrieve function called when the user selects a location
+export function Search(props) {
     return (
             <SearchBox 
                 accessToken={token.MAPBOX_TOKEN} 
-                onRetrieve={handleRetrieve} 
+                onRetrieve={
+                    (response) => {
+                        props.handleCoordinates(response.features[0].geometry.coordinates)
+                        console.log("handled coords");
+                    }
+                } 
                 options={{
                     proximity: [-122.30505981879918, 47.65331281821765 ]
                 }}
                 />
     );
-}
-
-function handleRetrieve(response) {
-    console.log(response.features[0].geometry.coordinates);
 }
