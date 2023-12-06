21bddf16fb26524619faf36a6e83aa3edf238325
aaminah <aaminah@cs.washington.edu>
2023-12-06 14:37:15 -0800

    final UI hopefully

--- src/DisplayLinks.js
+++ src/DisplayLinks.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import {getAccessMapLink} from "./directions";
 
 // coordinatesArray: an array of {latitude, longitude, distance (m), time (s), building} objects
@@ -20,4 +20,22 @@ export function DisplayLinks({ coordinatesArray, startCoordinates }) {
     ));
 
     return <ol>{links}</ol>;
+}*/
+
+import React from "react";
+import { getAccessMapLink } from "./directions";
+
+export function DisplayLinks({ coordinatesArray, startCoordinates }) {
+        const links = coordinatesArray.map(({ latitude, longitude, distance, time, building }, index) => (
+            <li key={index} className="card">
+                <div className="card-number">{index + 1}</div>
+                <h3>{building}</h3>
+                <p>Distance: <b>{Math.round(distance)} meters</b></p>
+                <a href={getAccessMapLink(startCoordinates, { latitude, longitude })} target="_blank" rel="noopener noreferrer">
+                    Directions to {building}
+                </a>
+            </li>
+        ));
+    
+        return <ol className="card-list">{links}</ol>;
 }
