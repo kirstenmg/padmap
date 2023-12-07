ae2cdf2f792cefc03d17071fc85764ae34d8c72d
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2023-12-06 22:47:02 -0800

    Remove commented-out code

--- src/DisplayLinks.js
+++ src/DisplayLinks.js
@@ -1,27 +1,3 @@
-/*import React from "react";
-import {getAccessMapLink} from "./directions";
-
-// coordinatesArray: an array of {latitude, longitude, distance (m), time (s), building} objects
-// startCoordinates: an object of {latitude, longitude}
-export function DisplayLinks({ coordinatesArray, startCoordinates }) {
-    // Map over the array to create links for each set of coordinates
-    const links = coordinatesArray.map(({ latitude, longitude, distance, time, building }, index) => (
-        <li>
-            <a
-                key={index}
-                href={getAccessMapLink(startCoordinates, { latitude, longitude })}
-                target="_blank"
-            >
-                Directions to {building}
-            </a>
-            <p> This location is approximately {Math.round(time / 60)} minutes away.
-                The distance is <b> {Math.round(distance)} meters </b>. </p>
-        </li>
-    ));
-
-    return <ol>{links}</ol>;
-}*/
-
 import React from "react";
 import { getAccessMapLink } from "./directions";
 
